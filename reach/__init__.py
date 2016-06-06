9736a208abff27dc2b05b09676893e1fd62b1785
stephantul <stephan@28101989.com>
2016-06-06 08:40:32 +0200

    updated init and setup
    updated init and setup

--- reach/__init__.py
+++ reach/__init__.py
@@ -1,3 +1,4 @@
+from __future__ import absolute_import
 from reach import reach
 
 __all__ = ['reach']
--- setup.py
+++ setup.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 """Setup file."""
 
 from setuptools import setup
@@ -5,7 +6,7 @@ from setuptools import find_packages
 
 
 setup(name='reach',
-      version='0.0.1',
+      version='0.0.2',
       description='A light-weight package for working with pre-trained word embeddings',
       author='Stéphan Tulkens',
       author_email='stephan.tulkens@uantwerpen.be',
@@ -13,4 +14,9 @@ setup(name='reach',
       license='MIT',
       packages=find_packages(exclude=['examples']),
       install_requires=['numpy>=1.11.0'],
+      classifiers=[
+          'Intended Audience :: Developers',
+          'Programming Language :: Python :: 2',
+          'Programming Language :: Python :: 3',],
+      keywords='word vectors natural language processing',
       zip_safe=True)
