e5ed0cc895d17429e797c6d7dd57bce82ff00d5d
stephantul <stephan.tulkens@uantwerpen.be>
2018-11-22 20:26:29 +0100

    bump version

--- reach/__init__.py
+++ reach/__init__.py
@@ -1,5 +1,5 @@
 """A package for reading and manipulating word embeddings."""
 from __future__ import absolute_import
-from reach.reach import Reach, Spreach
+from reach.reach import Reach
 
-__all__ = ['Reach', 'Spreach']
+__all__ = ['Reach']
