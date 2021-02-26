5b74584d99746142751ccd7e65b0045bbcfecf7e
Stephan Tulkens <stephantul@gmail.com>
2021-02-26 12:16:17 +0100

    Bump version, change E-mail

--- setup.py
+++ setup.py
@@ -5,19 +5,22 @@ from setuptools import setup
 from setuptools import find_packages
 
 
-setup(name='reach',
-      version='3.3.0',
-      description='A light-weight package for working with pre-trained'
-                  ' word embeddings',
-      author='Stéphan Tulkens',
-      author_email='stephan.tulkens@uantwerpen.be',
-      url='https://github.com/stephantul/reach',
-      license='MIT',
-      packages=find_packages(exclude=['examples']),
-      install_requires=['numpy>=1.11.0', 'tqdm', 'setuptools'],
-      classifiers=[
-          'Intended Audience :: Developers',
-          'Programming Language :: Python :: 2',
-          'Programming Language :: Python :: 3'],
-      keywords='word vectors natural language processing',
-      zip_safe=True)
+setup(
+    name="reach",
+    version="3.4.0",
+    description="A light-weight package for working with pre-trained"
+    " word embeddings",
+    author="Stéphan Tulkens",
+    author_email="stephantul@gmail.com",
+    url="https://github.com/stephantul/reach",
+    license="MIT",
+    packages=find_packages(exclude=["examples"]),
+    install_requires=["numpy>=1.11.0", "tqdm", "setuptools"],
+    classifiers=[
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 3",
+    ],
+    keywords="word vectors natural language processing",
+    zip_safe=True,
+)
