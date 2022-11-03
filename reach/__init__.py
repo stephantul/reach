8e5ea272bea11340abf768933e8ecb9625fd1111
stephantul <stephantul@gmail.com>
2022-11-03 09:02:27 +0100

    Rip out poetry, fix packaging, remove requirements.txt

--- reach/__init__.py
+++ reach/__init__.py
@@ -1,6 +1,10 @@
 """A package for reading and manipulating word embeddings."""
 from __future__ import absolute_import
 from reach.reach import Reach, normalize
-from reach.autoreach import AutoReach
 
-__all__ = ["Reach", "AutoReach", "normalize"]
+try:
+    from reach.autoreach import AutoReach  # noqa
+
+    __all__ = ["Reach", "normalize", "AutoReach"]
+except ImportError:
+    __all__ = ["Reach", "normalize"]
