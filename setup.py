2f8d59f4a895a53d48451497da6cef6a2f6baabc
stephantul <stephantul@gmail.com>
2023-07-20 11:34:31 +0200

    Update version

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="reach",
-    version="4.1.0",
+    version="4.1.1",
     description="A light-weight package for working with pre-trained word embeddings",
     author="Stéphan Tulkens",
     author_email="stephantul@gmail.com",
