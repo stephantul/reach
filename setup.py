a7a659298e9b3b7c2aa21a1e1647d1ea125c3a57
stephantul <stephantul@gmail.com>
2024-07-13 08:28:25 +0200

    remove 3.8 & 3.9 from supported versions

--- setup.py
+++ setup.py
@@ -23,8 +23,6 @@ setup(
     classifiers=[
         "Intended Audience :: Science/Research",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.8",
-        "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
         "Programming Language :: Python :: 3.11",
         "License :: OSI Approved :: MIT License",
